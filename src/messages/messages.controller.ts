ef519a25c6d25c76fa34861ba6f6459bd8b80e7a
rajperficient01 <rajl.perficient@gmail.com>
2023-11-20 23:34:35 +0530

    first commit

---
+++ src/messages/messages.controller.ts
@@ -0,0 +1,28 @@
+import { Controller, Get, Post, Body, Param } from '@nestjs/common';
+import { MessagesService } from './messages.service';
+import { createMessageDto } from './dtos/create.message.dto';
+
+@Controller('messages')
+export class MessagesController {
+    messagesService: MessagesService;
+    constructor() {
+        // DONT DO THIS ON REAL APP
+        // USE DEPENDENCY INJECTION
+        this.messagesService = new MessagesService();
+    }
+
+    @Get()
+    listMessages(){
+        return this.messagesService.findAll();
+    }
+
+    @Post()
+    createMessage(@Body() body: createMessageDto){
+        return this.messagesService.create(body.content);
+    }
+
+    @Get('/:id')
+    getMessage(@Param() id: string){
+        return this.messagesService.findOne(id);
+    }
+}
---
+++ src/messages/messages.repository.ts
@@ -0,0 +1,34 @@
+// import helper function from nodejs library
+import { readFile, writeFile} from 'fs/promises';
+
+export class MessagesRepository {
+    async findOne(id: string) {
+        //read file content from messages.json
+        const contents = await readFile('messages.json', 'utf8');
+        //content return JSON as a plain string we need to parse that string into objec
+        const messages = JSON.parse(contents);
+        //get specific message from id
+        console.log(`id = ${typeof id}`);
+        console.log(`content = ${contents}`);
+        console.log(`messages = ${typeof messages}`);
+        return messages[557];
+    }
+
+    async findAll() {
+        const contents = await readFile('messages.json', 'utf8');
+        const messages = JSON.parse(contents);
+        return messages;
+    }
+
+    async create(content: string) {
+        // read file
+        const contents = await readFile('messages.json', 'utf8');
+        const messages = JSON.parse(contents);
+        // randomly generate brand new ID
+        const id = Math.floor(Math.random() * 999);
+        // add ID and messages into messages.json file
+        messages[id] = { id, content };
+        // messages[id] - turns the entire thing back to string and write it back to messages.json file
+        await writeFile('messages.json', JSON.stringify(messages));
+    }
+}
