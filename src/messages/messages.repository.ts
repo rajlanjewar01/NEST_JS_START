64d4b00d4a342bfc33204b73d55d574c270ac451
rajperficient01 <rajl.perficient@gmail.com>
2023-11-21 01:37:29 +0530

    adding get gile path logic

--- src/messages/messages.repository.ts
+++ src/messages/messages.repository.ts
@@ -23,7 +23,7 @@ export class MessagesRepository {
         // console.log(`33 ${process.cwd()}/src/messages/files/messages.json`);
         // const contents = await fs.readFile(`${process.cwd()}/src/messages/files/messages.json`, 'utf8');
 
-        const file = await fs.readFile(process.cwd() + '/src/messages/files/messages.json', 'utf8');
+        const file = await fs.readFile('/src/messages/files/messages.json', 'utf8');
         const messages = JSON.parse(file);
         return messages;
         // const contents = await fs.readFile(process.cwd() + './files/messages.json', 'utf8');
