78b11e9b0b6b68435ed841ff287d6ab0451beb86
rajperficient01 <rajl.perficient@gmail.com>
2023-11-21 00:49:22 +0530

    path change

--- src/messages/messages.repository.ts
+++ src/messages/messages.repository.ts
@@ -4,7 +4,7 @@ import { readFile, writeFile} from 'fs/promises';
 export class MessagesRepository {
     async findOne(id: string) {
         //read file content from messages.json
-        const contents = await readFile('./messages.json', 'utf8');
+        const contents = await readFile('messages.json', 'utf8');
         //content return JSON as a plain string we need to parse that string into objec
         const messages = JSON.parse(contents);
         //get specific message from id
@@ -15,20 +15,20 @@ export class MessagesRepository {
     }
 
     async findAll() {
-        const contents = await readFile('./messages.json', 'utf8');
+        const contents = await readFile('messages.json', 'utf8');
         const messages = JSON.parse(contents);
         return messages;
     }
 
     async create(content: string) {
         // read file
-        const contents = await readFile('./messages.json', 'utf8');
+        const contents = await readFile('messages.json', 'utf8');
         const messages = JSON.parse(contents);
         // randomly generate brand new ID
         const id = Math.floor(Math.random() * 999);
         // add ID and messages into messages.json file
         messages[id] = { id, content };
         // messages[id] - turns the entire thing back to string and write it back to messages.json file
-        await writeFile('./messages.json', JSON.stringify(messages));
+        await writeFile('messages.json', JSON.stringify(messages));
     }
 }
