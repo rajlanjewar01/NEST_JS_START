233ef1252f5c5b379720575f5cbbd1d0712c7a8c
rajperficient01 <rajl.perficient@gmail.com>
2023-11-21 16:40:16 +0530

    test changes

--- src/messages/messages.repository.ts
+++ src/messages/messages.repository.ts
@@ -1,45 +1,43 @@
-// import helper function from nodejs library
-import { readFile, writeFile} from 'fs/promises';
-import { promises as fs } from 'fs';
+/**
+ * Add this class to dependency injection container
+ * 
+ */
 
+//1. import Injectable
+import { Injectable } from '@nestjs/common';
+import { readFile, writeFile} from 'fs/promises';
+
+//2. apply Injectable decorator
+@Injectable()
 export class MessagesRepository {
-    async findOne(id: string) {
-        //read file content from messages.json
-        const contents = await readFile('messages.json', 'utf8');
-        //content return JSON as a plain string we need to parse that string into objec
-        const messages = JSON.parse(contents);
-        //get specific message from id
-        console.log(`id = ${typeof id}`);
-        console.log(`content = ${contents}`);
-        console.log(`messages = ${typeof messages}`);
-        return messages[557];
+    async findOne(id :string) {
+        // read specific content
+        const contents = await readFile('messages.json','utf8');
+        //parse json into object
+        const messages = contents;
+        return messages[id]
     }
 
     async findAll() {
-        // const file = await fs.readFile(process.cwd() + './messages.json', 'utf8');
-        // const contents = await readFile('messages.json', 'utf8');
-        // console.log(`${process.cwd()}/src/messages/files/messages.json`);
-        // console.log(`11 ${process.cwd()}`);
-        // console.log(`33 ${process.cwd()}/src/messages/files/messages.json`);
-        // const contents = await fs.readFile(`${process.cwd()}/src/messages/files/messages.json`, 'utf8');
-        // console.log(process.cwd());
-        // const file = await fs.readFile(process.cwd() + '/src/messages/files/messages.json', 'utf8');
-        // const messages = JSON.parse(file);
-        return process.cwd();
-        // const contents = await fs.readFile(process.cwd() + './files/messages.json', 'utf8');
-        // const messages = JSON.parse(contents);
-        // return messages;
+        //read all content of file
+          // read all content of file
+          const contents = await readFile('messages.json', 'utf8');
+          // parse json into array of objects
+          const messages = JSON.parse(contents);
+          return messages;
     }
 
+    //FIXME
     async create(content: string) {
         // read file
-        const contents = await readFile('messages.json', 'utf8');
-        const messages = JSON.parse(contents);
-        // randomly generate brand new ID
+        const contents = readFile('messages.json','utf8');
+        // randomly generate ID
         const id = Math.floor(Math.random() * 999);
-        // add ID and messages into messages.json file
+        // get messages content and put into messages object
+        const messages = JSON.parse(content);
         messages[id] = { id, content };
-        // messages[id] - turns the entire thing back to string and write it back to messages.json file
+        // write the messages to the file
         await writeFile('messages.json', JSON.stringify(messages));
+        return messages[id];
     }
 }
